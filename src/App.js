acfeeb3eda7109674f5bed3c15d46faeb56f37f3
Lungi <149015152+lungi-kaptein@users.noreply.github.com>
2024-09-05 19:32:46 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Lungi Kaptein and is {" "}
+        <a href="https://github.com/lungi-kaptein/weather-react" target="_blank"> 
+        open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }

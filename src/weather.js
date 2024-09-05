83443e4d42aba91870f132951c49a53231ed37e9
Lungi <149015152+lungi-kaptein@users.noreply.github.com>
2024-09-06 00:47:50 +0200

    Added CSS

--- src/weather.js
+++ src/weather.js
@@ -10,10 +10,11 @@ export default function Weather() {
         type="search"
         placeholder="Enter a city.."
         className="form-control"
+        autoFocus="on"
         />
         </div>
         <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary"/>
+        <input type="submit" value="Search" className="btn btn-primary w-100"/>
         </div>
         </div>
        </form>
@@ -22,12 +23,17 @@ export default function Weather() {
         <li>Thursday 08:00</li>
         <li>Clear with periodic clouds</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
+            <div className="clearfix">
             <img 
             src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="Clear with periodic clouds"/>
-            10°C
+            alt="Clear with periodic clouds"
+            className="float-left"/>
+            <div className="float-left">
+            <span className="temperature">10</span><span className="unit">°C</span>
+            </div>
+            </div>
         </div>
         <div className="col-6">
             <ul>
